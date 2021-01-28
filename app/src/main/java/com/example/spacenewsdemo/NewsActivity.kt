9aa33910920a7a529143fa58ad026b7c7036a59d
Patrick S <18218018@std.stei.itb.ac.id>
2021-01-28 15:31:47 +0700

    change variables name, create new class: NetworkApiClient

--- app/src/main/java/com/example/spacenewsdemo/NewsActivity.kt
+++ app/src/main/java/com/example/spacenewsdemo/NewsActivity.kt
@@ -2,33 +2,26 @@ package com.example.spacenewsdemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import com.bumptech.glide.Glide
-import com.example.spacenewsdemo.databinding.ActivityMainBinding
 import com.example.spacenewsdemo.databinding.ActivityNewsBinding
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
-import io.reactivex.rxjava3.core.Single
 import io.reactivex.rxjava3.schedulers.Schedulers
 
 class NewsActivity : AppCompatActivity() {
-    private lateinit var b: ActivityNewsBinding
+    private lateinit var binding: ActivityNewsBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        b = ActivityNewsBinding.inflate(layoutInflater)
-        setContentView(b.root)
-        var id = intent.getStringExtra("ID")
+        binding = ActivityNewsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val id = intent.getStringExtra("ID")
         if (id != null) {
-            getNews(id).subscribeOn(Schedulers.newThread())
+            NetworkApiClient.getNewsDetail(id).subscribeOn(Schedulers.newThread())
                 .observeOn(AndroidSchedulers.mainThread()).subscribe({
-                    Glide.with(applicationContext).load(it.imageUrl).into(b.imageView1)
-                    b.textView1.text = it.title
-                    b.textView2.text = it.summary
+                    Glide.with(applicationContext).load(it.imageUrl).into(binding.imageView1)
+                    binding.textView1.text = it.title
+                    binding.textView2.text = it.summary
                 }, {})
         }
-    }
-
-    fun getNews(id: String): Single<News> {
-        return MainActivity.retrofit.getNewsDetail(MainActivity.API_KEY, id)
     }
 }
